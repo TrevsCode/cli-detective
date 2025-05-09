63eaf39042e08d1a39dc66bd3826c7e27690a43c
TrevsCode <Trevorscottherndon@gmail.com>
2025-05-09 11:58:58 -0400

    adding my list_cases method

--- investigations.py
+++ investigations.py
@@ -5,4 +5,9 @@ class Investigations:
     def add_case(self, case):
         self.cases.append(case)
         
-        
+    def list_cases(self):
+        open_cases = []
+        for case in self.cases:
+            if case.status == "open":
+                open_cases.append(case)
+        return open_cases
